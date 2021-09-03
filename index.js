9789dfc2b32fa7165a42a89f2d63d5d43bbb1bb0
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-09-02 20:41:02 -0500

    deployable

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const server = require('express')()
+const express = require('express')
+const cors = require('cors')
 
-server.listen(5000, () => {
-  console.log('listening on port 5000')
+console.log(process.env.USER)
+console.log(process.env.HOME)
+console.log(process.env.POSRT)
+
+const PORT = process.env.PORT || 5000
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/', (req, res) => {
+  res.send('<h1>hello</h1>')
+})
+
+server.get('/api', (req, res) => {
+  res.json({ messgae: 'hello there' })
+})
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
 })
