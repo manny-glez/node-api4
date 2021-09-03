b20bbb31da44bf2018bb3bb5859d0b2d09d04d1d
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-09-02 22:02:49 -0500

    Adds get users endpoint

--- index.js
+++ index.js
@@ -18,6 +18,39 @@ server.get('/api', (req, res) => {
   res.json({ messgae: 'hello there' })
 })
 
+// get users
+const initializeUsers = () => ([
+  { id: 1,name: 'john'},
+  { id: 2, name: 'paul'},
+  { id: 3, name: 'george'},
+  { id: 4, name: 'ringo' }
+])
+
+let users = initializeUsers()
+
+const find = () => {
+  return Promise.resolve(users)
+}
+
+server.get('/api/users', (req, res) => {
+  find()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(err => {
+      res.status(500).json({
+        messgae: 'error getting users',
+        err: err.messgae,
+      })
+    })
+})
+
+server.use('*', (req, res) => {
+  res.status(404).json({
+    messgae: 'not found'
+  })
+})
+
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 })
